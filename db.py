f205a341380c22accdc9729a7901d61c9bd5796f
avemooneez <mooneez.mnz@gmail.com>
2024-09-21 11:53:25 +0300

    Шаблонное заполнение класса Database в файле db.py

---
+++ db.py
@@ -0,0 +1,100 @@
+import psycopg2
+from psycopg2 import sql
+from utils.tokens import db_user, db_host, db_passwd
+
+class Database:
+    def __init__(self):
+        """
+        Инициализирует класс, подключается к БД.
+        """
+        
+        self.conn = psycopg2.connect(
+            user=f"{db_user}", password=f"{db_passwd}",
+            host=f"{db_host}", port="5432",
+            database="schoolproject"
+            )
+        self.cur = self.conn.cursor()
+
+    def start(self):
+        """
+        Создаёт таблицу users, если таковой не существует. Выводит всех пользователей в таблице.
+        """
+        
+        with self.conn:
+            self.create_tables()
+            self.get_db()
+
+    def create_tables(self):
+        self.cur.execute(
+            """
+CREATE TABLE IF NOT EXISTS users(
+    user_id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    is_active BOOLEAN DEFAULT TRUE,
+    grade INTEGER,
+    letter TEXT
+)
+"""
+        )
+
+    def get_db(self):
+        """
+        Выводит всю таблицу.
+        """
+        
+        with self.conn:
+            self.cur.execute("SELECT * FROM users")
+            users = self.cur.fetchall()
+            print(users)
+
+    def custom(self):
+        """
+        Специальная функция, применяется и изменяется при каких-либо тестах.
+        """
+        
+        with self.conn:
+            pass
+
+    def get_users(self):
+        """
+        Получает список всех пользователей из базы данных.
+
+        Возвращает:
+        list: Список кортежей, где каждый кортеж содержит идентификатор пользователя (user_id).
+
+        Примечание:
+        Если в базе данных нет пользователей, возвращается пустой список.
+        """
+        
+        with self.conn:
+            self.cur.execute("SELECT user_id FROM users")
+            return self.cur.fetchall()
+
+    def user_exists(self, user_id: int):
+        """
+        Проверяет, существует ли пользователь в базе данных по заданному идентификатору.
+
+        Параметры:
+        user_id (int): Идентификатор пользователя, который необходимо проверить.
+
+        Возвращает:
+        bool: True, если пользователь с указанным user_id существует в базе данных, иначе False.
+        """
+    
+        with self.conn:
+            self.cur.execute(
+                "SELECT * FROM users WHERE user_id = %s",
+                (user_id,)
+            )
+            return self.cur.fetchone() is not None
+
+    def add_user(self, user_id: int):
+        """
+        Добавляет нового пользователя в базу данных по заданному идентификатору.
+
+        Параметры:
+        user_id (int): Идентификатор пользователя, который необходимо добавить в базу данных.
+        """
+
+        with self.conn:
+            self.cur.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
