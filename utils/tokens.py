7c30a4cf5571ec302a0c594f9d3bcf82541a3238
avemooneez <mooneez.mnz@gmail.com>
2024-09-21 11:00:46 +0300

    Добавление токенов в tokens.py для подключения к БД

--- utils/tokens.py
+++ utils/tokens.py
@@ -3,3 +3,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 bot_token = os.getenv('BOT_TOKEN')
+db_user = os.getenv('DB_USER')
+db_host = os.getenv('DB_HOST')
+db_passwd = os.getenv('DB_PASSWD')
