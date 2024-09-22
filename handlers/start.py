3f5f32f3deea59a1ddb77313883d01f9fb5f402c
avemooneez <mooneez.mnz@gmail.com>
2024-09-22 10:22:31 +0300

    Изменение условия добавления ползователя в БД (было ошибочным)

--- handlers/start.py
+++ handlers/start.py
@@ -9,4 +9,6 @@ db = Database()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    if not db.user_exists(message.from_user.id):
+        db.add_user(message.from_user.id)
     await message.answer("Добро пожаловать в бота!")
