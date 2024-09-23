98eb69aa1f27312abf505c8fd7346df3ad226ec3
avemooneez <mooneez.mnz@gmail.com>
2024-09-23 17:00:13 +0300

    Создание функции ввода класса пользователя

--- handlers/start.py
+++ handlers/start.py
@@ -1,14 +1,31 @@
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.filters import Command
 from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.redis import RedisStorage
+from keyboards import grade_letter
 from db import Database
 
-
+storage = RedisStorage.from_url("redis://localhost:6379/0")
 router = Router()
 db = Database()
 
+class Grades(StatesGroup):
+    grade = State()
+    letter = State()
+
 @router.message(Command("start"))
-async def cmd_start(message: Message):
-    if not db.user_exists(message.from_user.id):
-        db.add_user(message.from_user.id)
+async def cmd_start(message: Message, state: FSMContext):
     await message.answer("Добро пожаловать в бота!")
+    if not db.user_exists(message.from_user.id):
+        await message.answer("Вы новый пользователь! Выберите ваш класс в клавиатуре ниже.", reply_markup=grade_letter.grade_kb())
+        await state.set_state(Grades.grade)
+
+@router.message(Grades.grade)
+async def grade_handler(message: Message, state: FSMContext):
+    grade = message.text
+    await state.update_data(grade=grade)
+    print(state.data)
+    await message.answer("Вы выбрали класс {}. Выберите букву класса ниже.".format(grade), reply_markup=grade_letter.letter_kb(grade=message.text))
+    await state.set_state(Grades.letter)
---
+++ keyboards/grade_letter.py
@@ -0,0 +1,35 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+def grade_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    for i in range(5, 12):
+        kb.add(KeyboardButton(text=str(i)))
+    kb.adjust(3)
+    return kb.as_markup(resize_keyboard=True)
+
+def letter_kb(grade) -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    match grade:
+        case '5':
+            for letter in ['A', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'И']:
+                kb.add(KeyboardButton(text=letter))
+        case '6':
+            for letter in ['А', 'Б', 'В', 'Г', 'Д', 'З', 'Ж']:
+                kb.add(KeyboardButton(text=letter))
+        case '7':
+            for letter in ['А', 'Б', 'В', 'Г', 'Д', 'Е']:
+                kb.add(KeyboardButton(text=letter))
+        case '8':
+            for letter in ['А', 'Б', 'В', 'Г', 'Д', 'Е']:
+                kb.add(KeyboardButton(text=letter))
+        case '9':
+            for letter in ['А', 'Б', 'В', 'Г', 'Д']:
+                kb.add(KeyboardButton(text=letter))
+        case '10':
+            for letter in ['А', 'Б']:
+                kb.add(KeyboardButton(text=letter))
+        case '11':
+            for letter in ['А', 'Б']:
+                kb.add(KeyboardButton(text=letter))
+    return kb.as_markup(resize_keyboard=True)
