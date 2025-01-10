1565a419b29f385b5cc889ad2f7e6ddbe57c799a
avemooneez <mooneez.mnz@gmail.com>
2025-01-10 21:03:45 +0300

    Рефакторинг и комментирование кода

--- bot.py
+++ bot.py
@@ -11,9 +11,8 @@ db = Database()
 schedule = SendScheduleImage()
 
 def schedule_start():
-    schedule.download_file(output_file=schedule.file_name)
-    print("Скачано расписание")
-    
+    schedule.download_file(output_file=schedule.file_name) # Функция скачивания расписания
+
 
 async def main():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
@@ -33,15 +32,15 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
 
     
-    if not os.path.exists('temp/schedule.docx'):
+    if not os.path.exists('temp/schedule.docx'): # Проверка наличия файла с расписанием
         schedule_start()
-    # Создаем две асинхронные задачи: для пулинга бота и для проверки расписания
+
     task_polling = asyncio.create_task(dp.start_polling(bot))
-    print("Started task 1")
     task_check_schedule = asyncio.create_task(schedule.check_new_schedule(bot=bot, db=db))
-    print("Starting task 2")
-    # Ждем, пока обе задачи не завершатся
-    await asyncio.gather(task_polling, task_check_schedule)
+    # Две асинхронных задачи, task_polling - long polling телеграм бота,
+    # task_check_schedule - бесконечная проверка на новое расписание
+
+    await asyncio.gather(task_polling, task_check_schedule) # Запуск двух async задач
 
 if __name__ == "__main__":
     asyncio.run(main())
